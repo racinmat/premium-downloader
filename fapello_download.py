a05fb644bff6fd16427a5e2a3a44d9bbf98c936a
Matěj Račinský <racinsky.matej@seznam.cz>
2022-07-31 19:47:31 +0200

    generalized download for images and videos

--- fapello_download.py
+++ fapello_download.py
@@ -5,6 +5,7 @@ import requests
 import yaml
 from lxml import html
 import progressbar
+import urllib.request
 
 hs = {
     'User-Agent': 'Mozilla/5.0',
@@ -13,16 +14,22 @@ hs = {
 }
 
 
-def download_creator(creator_name, assume_naming=True):
+def download_creator(creator_name, assume_naming=True, skip_failed=True):
     print(f'downloading {creator_name=}')
     s = requests.Session()
     s.headers.update(hs)
-
+    print(f'loading failed urls')
+    failed_txt = 'fapello_failed.txt'
+    if osp.exists(failed_txt):
+        with open(failed_txt, 'r', encoding='utf-8') as f:
+            failed_urls = set(map(lambda x: x.strip(), f.readlines()))
+    else:
+        failed_urls = set()
     save_dir = 'fapello'
-    base_url = 'https://fapello.com/{}/{}'
+    base_url = 'https://fapello.com/{}/{}/'
     widgets = [progressbar.Percentage(), ' ', progressbar.Counter(), ' ', progressbar.Bar(), ' ',
                progressbar.FileTransferSpeed()]
-    main_url = base_url[:-2].format(creator_name)
+    main_url = base_url[:-3].format(creator_name)
     response = s.get(main_url)
     if response.status_code == 302:
         print(f'creator page {main_url=} does not exist')
@@ -42,22 +49,34 @@ def download_creator(creator_name, assume_naming=True):
             save_path = osp.join(save_dir, creator_name, f'{creator_name}_{i:04}.jpg')
             if osp.exists(save_path):
                 continue
-        response = s.get(i_url)
+
+        if skip_failed and i_url in failed_urls:
+            continue
+        response = s.get(i_url, allow_redirects=False)
+        if response.status_code != 200:
+            print(f'non-existing page, storing to the list')
+            with open(failed_txt, 'a+', encoding='utf-8') as f:
+                f.write(i_url + '\n')
+            continue
+
         tree = html.fromstring(response.content)
         img_path = tree.xpath('//*[@id="wrapper"]/div[2]/div/div/div/div[2]/a/img')
         if len(img_path) == 0:
-            print(f'skipping page {i_url=}, probably invalid')
-            continue
-        image_url = img_path[0].attrib['src']
-        save_path = osp.join(save_dir, creator_name, osp.basename(image_url))
+            # maybe video
+            vid_path = tree.xpath('//*[@id="wrapper"]/div[2]/div/div/div/div[2]/video/source')
+            if len(vid_path) == 0:
+                print(f'skipping page {i_url=}, probably invalid')
+                continue
+            else:
+                target_url = vid_path[0].attrib['src']
+                save_path = osp.join(save_dir, creator_name, osp.basename(target_url))
+        else:
+            target_url = img_path[0].attrib['src']
+            save_path = osp.join(save_dir, creator_name, osp.basename(target_url))
         os.makedirs(osp.dirname(save_path), exist_ok=True)
         if osp.exists(save_path):
             continue
-        response = s.get(image_url)
-        if response.status_code != 200:
-            raise URLError
-        with open(save_path, 'wb') as f:
-            f.write(response.content)
+        urllib.request.urlretrieve(target_url, save_path)
     pbar.finish()
 
 
